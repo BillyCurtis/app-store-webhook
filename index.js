c9adea4e2b1f60541e4172d9623f4047ddc8b459
Billy <38408756+BillyCurtis@users.noreply.github.com>
2022-08-18 21:11:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { Collection, RichEmbed, Util, WebhookClient } = require("discord.js")
+const { Collection, EmbedBuilder, Util, WebhookClient } = require("discord.js")
 const fs = require("fs")
 const config = require("./config")
-const webhook = new WebhookClient(config.id, config.token, { disableEveryone: true })
+const webhook = new WebhookClient({ id: config.id, token: config.token })
 const store = require("app-store-scraper")
 const { EventEmitter } = require("events")
 let data = require("./data.json")
@@ -21,7 +21,7 @@ async function check() {
     for (const app of config.apps) {
         if (errors.get(app).error) continue
         try {
-            const res = await store.app({ id: app })
+            const res = await store.app({ id: app, country: "gb" })
             if (!data[res.id]) {
                 data[res.id] = {
                     id: res.id,
@@ -55,24 +55,24 @@ apps.on("update", async app => {
     await fs.writeFileSync("./data.json", JSON.stringify(data))
     console.log("[DATA] Wrote to data.json")
 
-    const embed = new RichEmbed()
-        .setAuthor(app.developer)
+    const embed = new EmbedBuilder()
+        .setAuthor({ name: app.developer, iconURL: app.icon })
         .setTitle(`${app.title} (${app.appId})`)
         .setURL(app.url)
         .setThumbnail(app.icon)
-        .setColor(0x7289DA)
-        .setFooter(app.updated)
-        .setTimestamp(app.updated)
-    
+        .setColor(0x1598f7)
+        .setFooter({ text: app.developer, iconURL: app.icon })
+        .setTimestamp(new Date(app.updated))
+
     if (app.releaseNotes.length > 1024) {
         const split = Util.splitMessage(app.releaseNotes, { maxLength: 1024 })
-        for (let i = 0; i < split.length; i++) embed.addField(i === 0 ? `v${app.version}` : "\u200B", split[i])
+        for (let i = 0; i < split.length; i++) embed.addFields(i === 0 ? `v${app.version}` : "\u200B", split[i])
     } else {
-        embed.addField(app.version, app.releaseNotes)
+        embed.addFields([{ name: `v${app.version}`, value: app.releaseNotes }])
     }
 
     console.log("[WEBHOOK] Sending message")
-    webhook.send("", { embeds: [embed] })
+    webhook.send({ embeds: [embed] })
      .then(msg => console.log("[WEBHOOK] Message sent!"))
      .catch(console.error)
 })
